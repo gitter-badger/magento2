f77e2fdd91053f44c8299ed9c5fea84a0f897988
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2015-02-11 15:28:41 +0200

    Support HHVM

--- dev/travis/hhvm/integration.sh
+++ dev/travis/hhvm/integration.sh
@@ -12,8 +12,6 @@ for (( i=0; i<$MAX_INSTANCE; i++ )); do
     cat ./etc/install-config-mysql.php | sed 's#magento_integration_tests#magento_integration_tests_${i}#' > ./etc/install-config-mysql-${i}.php
     mysql -uroot -e"create database magento_integration_tests_${i};"
 done
-ls -1 ./etc/
-ls -1 ./
 
 run_tests() {
     dir=$1
@@ -21,6 +19,7 @@ run_tests() {
     echo -e "\nRunning ${dir} tests"
     FOLDER=$(echo $dir | cut -d'/' -f4)
     RESULT="--stderr -c ${BASE_DIR}/dev/tests/integration/phpunit-${id}.xml --log-junit ${BASE_DIR}/integration_tests_${FOLDER}.xml"
+    cat ${BASE_DIR}/dev/tests/integration/phpunit-${id}.xml | grep TESTS_INSTALL_CONFIG_FILE
     fl="/tmp/_test_${id}.log"
     echo -e "\n $PHPUNIT_COMMAND $RESULT $dir \n"
     $PHPUNIT_COMMAND $RESULT $dir | tee $fl
@@ -29,7 +28,7 @@ run_tests() {
 }
 
 export -f run_tests
-ls -dX ./testsuite/Magento/* | grep -v _files | $BASE_DIR/dev/travis/parallel --gnu -P $MAX_INSTANCE 'run_tests {} {%}' || exit 1
+ls -dX ./testsuite/Magento/* | grep -v _files | $BASE_DIR/dev/travis/parallel --nn --gnu -P $MAX_INSTANCE 'run_tests {} {%}' || exit 1
 
 unset -f run_tests
 unset MAX_INSTANCES
