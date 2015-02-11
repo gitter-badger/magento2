100ca3fa752127bd7cd30d12b268bdcca4e759aa
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2015-02-11 13:30:17 +0200

    Support HHVM

--- dev/travis/hhvm/integration.sh
+++ dev/travis/hhvm/integration.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo 'alias phpunit="hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit"' >> ~/.bashrc
-. ~/.bashrc
-cd $TRAVIS_BUILD_DIR/dev/tests/integration/
-#hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=false $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit -c ./phpunit.xml.dist
-hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit --testsuite $TEST_SUITE_INT
-
 export MAX_INSTANCE=4
 export BASE_DIR=$TRAVIS_BUILD_DIR
 export PHPUNIT_COMMAND="hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit -- "
