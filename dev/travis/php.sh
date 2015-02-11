3ab18c1835b6401d5b85a68929a2e33cbaf03ccc
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2015-02-11 15:47:40 +0200

    Support HHVM

--- dev/travis/php.sh
+++ dev/travis/php.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 php -r "echo PHP_VERSION, PHP_EOL;"
+echo $TRAVIS_PHP_VERSION
 echo 'sendmail_path = "/usr/sbin/sendmail -t -i "' | sudo tee "/home/travis/.phpenv/versions/${PHPENV_VERSION}/etc/conf.d/sendmail.ini"
 echo '' > ~/.phpenv/versions/${PHPENV_VERSION}/etc/conf.d/xdebug.ini
 echo 'memory_limit = -1' >> ~/.phpenv/versions/${PHPENV_VERSION}/etc/conf.d/travis.ini
