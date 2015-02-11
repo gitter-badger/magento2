73de753db807385148efcda2c638790f7eed2327
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2015-02-11 13:50:48 +0200

    Support HHVM

--- dev/travis/hhvm/integration.sh
+++ dev/travis/hhvm/integration.sh
@@ -9,7 +9,7 @@ for (( i=0; i<$MAX_INSTANCE; i++ )); do
     cat ./phpunit.xml.dist \
     | sed 's#</php>#    <const name="TESTS_PARALLEL_THREAD" value="${i}"/>\n    </php>#' \
     | sed 's#etc/install-config-mysql.php#etc/install-config-mysql-${i}.php#' > ./phpunit-${i}.xml
-    cat ./etc/install-config-mysql.travis.php | sed 's#magento_integration_tests#magento_integration_tests_${i}#' > ./etc/install-config-mysql-${i}.php
+    cat ./etc/install-config-mysql.travis.php.dist | sed 's#magento_integration_tests#magento_integration_tests_${i}#' > ./etc/install-config-mysql-${i}.php
     mysql -uroot -e"create database magento_integration_tests_${i};"
 done
 
