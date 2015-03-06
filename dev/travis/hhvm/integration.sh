0ba491c661122d26c24dcebe7f20b2da8789865d
Oleksandr Ivashchenko <sanuch@gmail.com>
2015-03-07 00:34:29 +0200

    HHVM parallel

--- dev/travis/hhvm/integration.sh
+++ dev/travis/hhvm/integration.sh
@@ -9,7 +9,9 @@ for (( i=0; i<$MAX_INSTANCE; i++ )); do
     cat ./phpunit.xml.dist \
     | sed 's#TESTS_PARALLEL_THREAD" value="0#TESTS_PARALLEL_THREAD" value="'$i'#' \
     | sed "s#etc/install-config-mysql.php#etc/install-config-mysql-${i}.php#" > ./phpunit-${i}.xml
+
     cat ./etc/install-config-mysql.php | sed "s#magento_integration_tests#magento_integration_tests_${i}#" > ./etc/install-config-mysql-${i}.php
+
     mysql -uroot -e"create database magento_integration_tests_${i};"
 done
 rm -f ./phpunit.xml.dist
@@ -19,11 +21,9 @@ run_tests() {
     id=$(( $2 - 1 ))
     echo -e "\nRunning ${dir} tests"
     FOLDER=$(echo $dir | cut -d'/' -f4)
-    RESULT="-c ${BASE_DIR}/dev/tests/integration/phpunit-${id}.xml --log-junit ${BASE_DIR}/integration_tests_${FOLDER}.xml"
+    RESULT="--configuration ${BASE_DIR}/dev/tests/integration/phpunit-${id}.xml --log-junit ${BASE_DIR}/integration_tests_${FOLDER}.xml"
     fl="/tmp/_test_${id}.log"
     echo -e "\n $PHPUNIT_COMMAND $RESULT $dir \n"
-    cat ./etc/install-config-mysql-${id}.php | grep db_name
-    cat ./phpunit-${id}.xml | grep TESTS_PARALLEL_THREAD
     echo ""
     $PHPUNIT_COMMAND $RESULT $dir | tee $fl
     ls -l ./tmp/
