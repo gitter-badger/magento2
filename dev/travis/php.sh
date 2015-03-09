3422d721ddc7af8b3b297d146263ba9dded528c3
Aleksandr Ivashchenko <sanuch@gmail.com>
2015-03-09 17:54:54 +0200

    Debug php version

--- dev/travis/php.sh
+++ dev/travis/php.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "PHP Version"
 php -r "echo PHP_VERSION, PHP_EOL;"
+echo "Travis PHP verion"
 echo $TRAVIS_PHP_VERSION
 echo 'sendmail_path = "/usr/sbin/sendmail -t -i "' | sudo tee "/home/travis/.phpenv/versions/${PHPENV_VERSION}/etc/conf.d/sendmail.ini"
 echo '' > ~/.phpenv/versions/${PHPENV_VERSION}/etc/conf.d/xdebug.ini
